d512fee2d3cce1cf4cf3934f10c95d0c3fd55a7f
babitionr <nhuy0164217@gmail.com>
2025-04-30 09:04:07 +0700

    first commit

---
+++ src/main/java/com/repices/repices/RepicesApplication.java
@@ -0,0 +1,13 @@
+package com.repices.repices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepicesApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/repices/repices/pojo/Repices.java
@@ -0,0 +1,5 @@
+package com.repices.repices.pojo;
+
+public class Repices {
+    
+}
